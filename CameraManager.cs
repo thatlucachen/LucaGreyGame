4bfe487539195bebd4900299bfc19a958446d214
Luca Chen <lucachen101@gmail.com>
2024-06-10 20:18:45 +0700

    Create CameraManager.cs

---
+++ CameraManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject virtualCamera;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            virtualCamera.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            virtualCamera.SetActive(false);
+        }
+    }
+}
